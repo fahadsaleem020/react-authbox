7d9b03af3ab8c7f64cb3151aa025f865f23d2398
fahadsaleem020 <fahadsaleem@gmail.com>
2024-01-08 14:29:26 +0500

    with verifyemail

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { UserProvider } from "@components/userprovider";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
+//
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
